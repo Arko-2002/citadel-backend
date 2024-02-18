60863e5d877c30f54a86d7d5933f8e43c1956d8b
unknown <ayushsingh.8d@gmail.com>
2024-02-18 09:44:39 +0530

    fixed cors bug

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 dotenv.config();
 const app = express();
-app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
+app.use(cors({ origin: '*', credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
